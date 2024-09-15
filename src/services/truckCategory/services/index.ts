ba0c3e59e646dcfabd944230d4fa14ed80c952ca
guliyevdev <q.behruz1@gmail.com>
2024-09-15 15:52:56 +0400

    Add create method to TruckCategoryService

--- src/services/truckCategory/services/index.ts
+++ src/services/truckCategory/services/index.ts
@@ -5,5 +5,16 @@ export default class TruckCategoryService {
     return await db.truckCategory.findMany();
   }
 
-  
+  static async create(name: string, weight: number, imageUrl: string, capacity: number, length: number, width: number) {
+    return await db.truckCategory.create({
+      data: {
+        name,
+        weight,
+        imageUrl,
+        capacity,
+        length,
+        width,
+      },
+    });
+  }
 }
