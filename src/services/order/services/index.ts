ed820d05aeecc41a54b228cc6228283de8ab9e0e
guliyevdev <q.behruz1@gmail.com>
2024-10-29 18:02:30 +0400

    Refactor OrderService to include additional methods for creating, updating, and deleting orders, and retrieving orders by status and with pagination.

--- src/services/order/services/index.ts
+++ src/services/order/services/index.ts
@@ -1,9 +1,57 @@
 import { db } from '../../../utils/db.server';
-
+import { OrderStatus } from '@prisma/client';
 export default class OrderService {
+    // Yeni sifariş yaratmaq
     static async createOrder(order: any): Promise<any> {
         return await db.order.create({
             data: order
-        })
+        });
+    }
+
+    // Bütün sifarişləri əldə etmək
+    static async getAllOrders(): Promise<any[]> {
+        return await db.order.findMany();
+    }
+
+    // ID üzrə sifarişi əldə etmək
+    static async getOrderById(orderId: number): Promise<any | null> {
+        return await db.order.findUnique({
+            where: {
+                id: orderId
+            }
+        });
+    }
+
+    // Sifarişi yeniləmək
+    static async updateOrder(orderId: number, updatedData: any): Promise<any> {
+        return await db.order.update({
+            where: {
+                id: orderId
+            },
+            data: updatedData
+        });
+    }
+
+    // Sifarişi silmək
+    static async deleteOrder(orderId: number): Promise<any> {
+        return await db.order.delete({
+            where: {
+                id: orderId
+            }
+        });
+    }
+    static async getOrdersByStatus(status: OrderStatus): Promise<any[]> {
+        return await db.order.findMany({
+            where: {
+                status: status
+            }
+        });
+    }
+
+    static async getOrdersWithPagination(page: number, pageSize: number): Promise<any[]> {
+        return await db.order.findMany({
+            skip: (page - 1) * pageSize,
+            take: pageSize
+        });
     }
 }
